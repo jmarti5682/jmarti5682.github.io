02d038f736a742a70f659f6f1a0414fa9786bac5
Jose Martinez-Ponce <101851045+jmarti5682@users.noreply.github.com>
2025-06-20 22:57:32 -0400

    Update WelcomeSection.js

--- src/components/WelcomeSection.js
+++ src/components/WelcomeSection.js
@@ -1,1 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import './WelcomeSection.css';
 
+const greetings = [
+  'Bienvenidos',
+  'Bienvenue',
+  'Welcome',
+  'いらっしゃいませ'
+];
+
+export default function WelcomeSection() {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((index + 1) % greetings.length);
+    }, 2000);
+    return () => clearInterval(interval);
+  }, [index]);
+
+  return (
+    <div className="welcome">
+      <pre className="courier">{greetings[index]}</pre>
+    </div>
+  );
+}
